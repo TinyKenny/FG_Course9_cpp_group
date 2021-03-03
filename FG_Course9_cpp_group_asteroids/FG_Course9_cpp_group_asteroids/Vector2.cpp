2c63162c2faf7f7d3ae1aa85c5657869034d4d77
Jonathan Rawet <Jonathan@rawet.se>
2021-03-03 12:25:01 +0100

    adding some operators to vector2

--- FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/Vector2.cpp
+++ FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/Vector2.cpp
@@ -1,1 +1,38 @@
 #include "Vector2.h"
+
+Vector2& Vector2::operator+=(const Vector2& other)
+{
+	x += other.x;
+	y += other.y;
+	return *this;
+}
+
+Vector2& Vector2::operator-=(const Vector2& other)
+{
+	x -= other.x;
+	y -= other.y;
+	return *this;
+}
+
+Vector2 Vector2::operator-() const
+{
+	return { -x, -y };
+}
+
+Vector2 Vector2::operator+(const Vector2& other) const
+{
+	return { x + other.x, y + other.y };
+}
+
+Vector2 Vector2::operator-(const Vector2& other) const
+{
+	return { x - other.x, y - other.y };
+}
+
+
+
+
+
+
+
+
--- FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/Vector2.h
+++ FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/Vector2.h
@@ -6,6 +6,11 @@ struct Vector2
 public:
 	float x;
 	float y;
+	Vector2& operator+=(const Vector2& other);
+	Vector2& operator-=(const Vector2& other);
+	Vector2 operator-() const;
+	Vector2 operator+(const Vector2& other) const;
+	Vector2 operator-(const Vector2& other) const;
 };
 
 #endif // !VECTOR_2_H
