11f23261d94690f184e36d1b9dc3e14a346b5712
Jonathan Rawet <Jonathan@rawet.se>
2021-03-04 15:26:55 +0100

    replay prompt appears to work, check for memory leaks

--- FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/main.cpp
+++ FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/main.cpp
@@ -11,23 +11,52 @@
 
 #include <iostream>
 #include <string>
+//#include <random>
 
 
 
 /* TODO
 replay
 asteroids should be destroyed when hit by bullets
-actual game over when player is hit by an asteroid
 asteroid respawning
+player spaceship instantiate in middle of window
 (nice to have) bullets should do line collision test
 */
 
 int main(int argc, char** argv)
 {
+	//srand(time(NULL));
 
-	Application* myApp = Application::getInstace();
-	myApp->run();
-	delete myApp;
+	bool playGame = true;
+	while (playGame)
+	{
+		Application* myApp = Application::getInstace();
+		myApp->run();
+		delete myApp;
+
+		std::cout << "Play again? y/n" << std::endl;
+		bool playerHasGivenValidAnswer = false;
+		while (!playerHasGivenValidAnswer)
+		{
+
+			std::string playerInput;
+			std::getline(std::cin, playerInput);
+
+			if (playerInput == "y")
+			{
+				playerHasGivenValidAnswer = true;
+			}
+			else if (playerInput == "n")
+			{
+				playerHasGivenValidAnswer = true;
+				playGame = false;
+			}
+			else
+			{
+				std::cout << "invalid input, please input \"y\" or \"n\"" << std::endl;
+			}
+		}
+	}
 
 	return 0;
 }
