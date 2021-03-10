1e42dc5bdd864534501e79f63bfa8a16d5a26d67
Jonathan Rawet <Jonathan@rawet.se>
2021-03-10 17:05:39 +0100

    updating TODO

--- FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/src/main.cpp
+++ FG_Course9_cpp_group_asteroids/FG_Course9_cpp_group_asteroids/src/main.cpp
@@ -9,7 +9,7 @@ Fonts and scoring system: implemented
 	(UFO and Asteroid use a lot of duplicated code in Application and PhysicsSystem atm)
 	constantly changes its velocity so that it always moves towards the player
 State machine for controlling game states: implemented
-Particle system: WIP
+Particle system: implemented (could use some improvements)
 Resource pools: WIP
 Sound (option): not done
 Optimized collision detection (option): Wont be implemented
